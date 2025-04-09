2c2f3d1f323e520777b5983d187aad12b9476c06
Arber <arber.kulla888@gmail.com>
2025-04-10 00:59:10 +0200

    Added infinite scrolling

--- src/data/moviesSlice.js
+++ src/data/moviesSlice.js
@@ -1,9 +1,18 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 
-export const fetchMovies = createAsyncThunk('fetch-movies', async (apiUrl) => {
-    const response = await fetch(apiUrl)
-    return response.json()
-})
+export const fetchMovies = createAsyncThunk('fetch-movies', async (apiUrl, { rejectWithValue }) => {
+    try {
+      const response = await fetch(apiUrl)
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`)
+      }
+      const data = await response.json()
+      return data
+    } catch (err) {
+      return rejectWithValue(err.message)
+    }
+  })
+  
 
 const moviesSlice = createSlice({
     name: 'movies',
@@ -14,8 +23,12 @@ const moviesSlice = createSlice({
     reducers: {},
     extraReducers: (builder) => {
         builder.addCase(fetchMovies.fulfilled, (state, action) => {
-            state.movies = action.payload
-            state.fetchStatus = 'success'
+            if (action.meta.arg.includes('&page=1') || !action.meta.arg.includes('&page=')) {
+                state.movies = action.payload
+            }  else {  
+                state.movies.results = [...state.movies.results,...action.payload.results]
+                state.fetchStatus = 'success'
+            }
         }).addCase(fetchMovies.pending, (state) => {
             state.fetchStatus = 'loading'
         }).addCase(fetchMovies.rejected, (state) => {
