8cf332db53262a5b85db744945a45f76dc1d2f54
Arber <arber.kulla888@gmail.com>
2025-04-14 15:32:14 +0200

    U shtua nje toast kur filmat nuk kan trailer.

--- src/components/YoutubePlayer.jsx
+++ src/components/YoutubePlayer.jsx
@@ -1,42 +1,43 @@
 import ReactPlayer from 'react-player'
-import 'reactjs-popup/dist/index.css'
 import Popup from 'reactjs-popup'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
 
 import '../styles/youtubeplayer.scss'
 
-
 const YoutubePlayer = ({ videoKey, isOpen, setOpen }) => {
-  const [loading, setLoading] = useState(true); 
+  const [loading, setLoading] = useState(true);
 
-  if (!isOpen) {
-    return null;
-  }
-
+  useEffect(() => {
+    if (isOpen && videoKey === null) {
+      toast.error('Sorry, trailer unavailable.')
+      setOpen(false)
+    }
+  }, [isOpen, videoKey, setOpen])
 
   const closeLogic = () => {
-    setOpen(false);
-    setLoading(true);
+    setOpen(false)
+    setLoading(true)
   }
+
+  if (!isOpen || videoKey === null) return null
 
   return (
     <div className="modal">
-      <Popup open={isOpen} onClose={() => closeLogic()} modal nested>
+      <Popup open={isOpen} onClose={closeLogic} modal nested>
         <div className="modal-content">
-          <button className="close-button" onClick={() => closeLogic()}> &times; </button>
+          <button className="close-button" onClick={closeLogic}> &times; </button>
           <ReactPlayer 
             className={`video-player ${loading ? 'loading' : ''}`}
             url={`https://www.youtube.com/watch?v=${videoKey}`} 
-            controls={true}
-            playing={true}
+            controls
+            playing
             data-testid="youtube-player"
           />
         </div>
       </Popup>
-      </div>  
-      );
+    </div>
+  )
 }
 
-
-
-export default YoutubePlayer;
+export default YoutubePlayer
